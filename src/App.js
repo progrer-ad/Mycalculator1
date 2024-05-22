127a015eaae7c2eb52785bf2f2c6e12e5d13c0a1
Sadulla <114229458+progrer-ad@users.noreply.github.com>
2024-05-22 22:50:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,11 +18,61 @@ function App() {
 
   const calculate = () => {
     try {
-      setResult(eval(result).toString());
+      setResult(evaluateExpression(result));
     } catch {
-      setResult("Error")
+      setResult("Error");
     }
   }
+
+  const evaluateExpression = (expression) => {
+    const ops = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+    };
+
+    const values = [];
+    const operators = [];
+
+    const precedence = (op) => {
+      if (op === '+' || op === '-') return 1;
+      if (op === '*' || op === '/') return 2;
+      return 0;
+    };
+
+    const applyOp = () => {
+      const b = values.pop();
+      const a = values.pop();
+      const op = operators.pop();
+      values.push(ops[op](a, b));
+    };
+
+    let i = 0;
+    while (i < expression.length) {
+      if (!isNaN(expression[i])) {
+        let value = '';
+        while (i < expression.length && (!isNaN(expression[i]) || expression[i] === '.')) {
+          value += expression[i++];
+        }
+        values.push(parseFloat(value));
+      } else if (expression[i] in ops) {
+        while (operators.length && precedence(operators[operators.length - 1]) >= precedence(expression[i])) {
+          applyOp();
+        }
+        operators.push(expression[i]);
+        i++;
+      } else {
+        i++;
+      }
+    }
+
+    while (operators.length) {
+      applyOp();
+    }
+
+    return values[0].toString();
+  };
 
   return (
     <div className="App">
@@ -30,11 +80,11 @@ function App() {
 
       <div className='container'>
         <form>
-          <input type="text" value={result} />
+          <input type="text" value={result} readOnly />
         </form>
 
         <div className='keypad'>
-        <button name="1" onClick={handleClick}>1</button>
+          <button name="1" onClick={handleClick}>1</button>
           <button name="2" onClick={handleClick}>2</button>
           <button name="3" onClick={handleClick}>3</button>
           <button name="+" onClick={handleClick} className='highlight'>+</button>
@@ -54,9 +104,7 @@ function App() {
           <button onClick={handleDelete} className='highlight'>DEL</button>
           <button onClick={calculate} id='equal' className='highlight'>=</button>
         </div>
-
       </div>
-
     </div>
   );
 }
